7c39b0672ab94a8c28ab3d04b0788b617c2594aa
0xredshadow <alblowy1@gmail.com>
2023-04-27 00:14:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,33 +5,16 @@ API_TOKEN = 'Token here bro'
 MYBOT = Bot(API_TOKEN)
 dp = Dispatcher(MYBOT)
 ###########################################################
-# /start
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-	username = message.from_user.username
-	msg = f"<b>Hello</b>, <i>{username}!</i>\n For Help use /help "
-	await MYBOT.send_message(message.chat.id, msg, parse_mode='html')
-###########################################################
-# /whoami
-@dp.message_handler(commands=['whoami'])
-async def whoami(message: types.Message):
-	username = message.from_user.username
-	id = message.from_user.id
-	whoami_msg = f"username: {username}\nid: {id}"
-	await MYBOT.send_message(message.chat.id, whoami_msg)
-###########################################################
 # /help
 @dp.message_handler(commands=['help'])
 async def help(message: types.Message):
-	text_help = f'/start - Start \n' \
-	            f'/whoami - Show Username and id \n' \
-	            f'/help - Just For Help \n' \
-	            f'/ask - just ask bot i dont have answer lol\n' 
+	text_help = f'/help - For Help \n' \
+	            f'/ask -  example /ask 2+2?\n' 
 	await MYBOT.send_message(message.chat.id, text_help, parse_mode='html')
 ###########################################################
-# /ask <<<< this for you ,, try write like /start /whoami /help ... it is easy
+@dp.message_handler() # For any text in chat
+async def echo(message: types.Message):
+    await message.answer('[+] Wellcome To RedShadow Bot :) use /help') # Get Only Your Username
 ###########################################################
-
 if __name__ == '__main__':
 	executor.start_polling(dp, loop=True)
-

6a49aa4a783da889d691961838492114cddade25
0xredshadow <alblowy1@gmail.com>
2023-04-26 23:11:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ async def echo(message: types.Message):
 
 ##########################################################
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, one_time=False)
+    executor.start_polling(dp, skip_updates=True)

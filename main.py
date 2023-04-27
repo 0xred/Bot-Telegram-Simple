a3e33a260baec7144e17a0ac84445d083a481790
0xredshadow <alblowy1@gmail.com>
2023-04-27 11:36:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from aiogram import Bot, Dispatcher, types, executor
-from asyncio import sleep
 import openai
 
 ###########################################################
-API_TOKEN = 'Token Telegram Bot' 
-openai.api_key = "Openai API"
+API_TOKEN = 'Token telegram' 
+openai.api_key = "Token Openai"
 MYBOT = Bot(API_TOKEN)
 dp = Dispatcher(MYBOT)
 ###########################################################
-# /help
-@dp.message_handler(commands=['help'])
+# /start
+@dp.message_handler(commands=['start'])
 async def help(message: types.Message):
 	await MYBOT.send_message('[+] Bot GPT By RedShadow @WTTTF')
 ###########################################################
-@dp.message_handler() # For any text in chat ... YOUR ASK
+@dp.message_handler() # For any text in chat : Your Ask Ai
 async def echo(message: types.Message):
 	mytext = message.text 
 	response = openai.ChatCompletion.create(
 		model="gpt-3.5-turbo",
 		messages=[{"role":"user","content": mytext}]
 		)
-	aichat = response.choices[0].message.content
+	aichat = response.choices[0].message.content 
 	await message.answer(aichat) # Answer Ai
 ###########################################################
 if __name__ == '__main__':

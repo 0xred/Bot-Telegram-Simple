ea9f593261252656da167b9cfa3f0f558498facf
0xredshadow <alblowy1@gmail.com>
2023-04-30 23:29:15 +0300

    Update TestButtonOptioninTelebot.py

--- TestButtonOptioninTelebot.py
+++ TestButtonOptioninTelebot.py
@@ -4,7 +4,7 @@ TELEGRAM_TOKEN = '1940813409:AAFcDNxju0PpzKxB4u-VHpDGAn6eH2c7VkQ'
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
 #########################################################################
-# Button
+# Show Button
 def gen_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
@@ -14,16 +14,17 @@ def gen_markup():
     )
     return markup
 #########################################################################
-# Handle Option using call.data .. exampe :cb_op1 .. 
+# After Press Button ..
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "cb_op1":
-        bot.answer_callback_query(call.id, "Choice Option 1")
+        bot.answer_callback_query(call.id, "You Choice Option 1")
     elif call.data == "cb_op2":
-        bot.answer_callback_query(call.id, "Choice Option 2")
+        bot.answer_callback_query(call.id, "You Choice Option 2")
 #########################################################################
+# First Messages
 @bot.message_handler(func=lambda message: True)
 def message_handler(message):
-    bot.send_message(message.chat.id, "Choice Option ???????", reply_markup=gen_markup())  #  Yes/No?
+    bot.send_message(message.chat.id, "Choice ???????", reply_markup=gen_markup())
 #########################################################################
 bot.infinity_polling()
